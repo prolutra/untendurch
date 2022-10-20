2741a2f2f86a8a180e20ca7aa7f51c98a5bec310
Thomas Rawyler <thomas.rawyler@tegonal.com>
2022-10-20 15:55:30 +0200

    removed ci-create-npmrc

--- frontend/scripts/ci-create-npmrc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-current_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)"
-cd "$current_dir/.."
-
-echo 'registry=https://nexus.tegonal.com/repository/npm-proxy/' > ~/.npmrc
-encoded=$(echo -n "$NEXUS_USERNAME:$NEXUS_PASSWORD" | base64)
-echo "_auth=$encoded"  >> ~/.npmrc
